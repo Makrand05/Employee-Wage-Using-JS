491637e560e3eebfa13994a8f964f981eca15733
Makrand <makrandshingare@gmail.com>
2022-01-19 07:58:56 +0530

    Added : Store the Daily Wage along with the Total Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -3,25 +3,53 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS=20;
-const MAX_HRS_IN_MONTH=100;
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+
+let empDailyWageArray = new Array();
+let empDailyWageMap=new Map();
+let empDailyWorkingHrs=new Map();
 
 function getWorkingHours(empCheck) {
-    switch (empCheck) {
-        case IS_PART_TIME:
-            return PART_TIME_HOURS;
-        case IS_FULL_TIME:
-            return FULL_TIME_HOURS;
-        default:
-            return 0;
-    }
+  switch (empCheck) {
+    case IS_PART_TIME:
+      return PART_TIME_HOURS;
+    case IS_FULL_TIME:
+      return FULL_TIME_HOURS;
+    default:
+      return 0;
+  }
 }
-    let totalEmpHrs = 0;
-    let totalWorkingDays = 0;
-    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-        totalWorkingDays++;
-        let empCheck = Math.floor(Math.random() * 10) % 3;
-        totalEmpHrs += getWorkingHours(empCheck);
-    }
-    let empWage = totalEmpHrs * WAGE_PER_HOUR;
-    console.log("UC5 -Total Days:" + totalWorkingDays + " Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
+
+function calculateDailyWage(empHours) {
+  return empHrs * WAGE_PER_HOUR;
+}
+
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+while (
+  totalEmpHrs <= MAX_HRS_IN_MONTH &&
+  totalWorkingDays < NUM_OF_WORKING_DAYS
+) {
+  totalWorkingDays++;
+  let empCheck = Math.floor(Math.random() * 10) % 3;
+  empHrs = getWorkingHours(empCheck);
+  empDailyWageArray.push(calculateDailyWage(empHrs));
+  totalEmpHrs += empHrs;
+
+  empDailyWageMap.set(totalWorkingDays,calculateDailyWage(empHrs));
+  empDailyWorkingHrs.set(totalWorkingDays,getWorkingHours(empCheck));
+}
+let empWage = calculateDailyWage(totalEmpHrs);
+console.log(
+  "UC6 -Total Days:" +
+    totalWorkingDays +
+    " Total Hours:" +
+    totalEmpHrs +
+    " Employee Wage:" +
+    empWage
+);
+// Store in Daliy wage 
+console.log(empDailyWageArray);
+
+
