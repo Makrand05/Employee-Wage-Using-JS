b6a98e9574e5ed3eec30ea504198ed17dc78fafe
Makrand <makrandshingare@gmail.com>
2022-01-23 12:34:32 +0530

    Perform following Object operations using Arrow Functions

--- JSObject.js
+++ JSObject.js
@@ -42,3 +42,32 @@ while (   totalEmpHrs <= MAX_HRS_IN_MONTH &&   totalWorkingDays < NUM_OF_WORKING
 }
 console.log(empDailyHrsArray);
 
+// UC 11 A Calc total Wage and total hours worked
+
+let totalWage=empDailyHrsArray.reduce((totalWage,DailyWageAndHrsObj)=>{
+  return totalWage+=DailyWageAndHrsObj.dailyWage;
+},0)
+console.log(" UC 11 A Total Wage "+totalWage);
+
+let totalHours=empDailyHrsArray.reduce((totalHours,DailyWageAndHrsObj)=>{
+  return totalHours+=DailyWageAndHrsObj.dailyHours;
+},0)
+console.log("Total Hours "+totalHours);
+
+//UC 11 B Show the full workings days using foreach
+console.log("UC 11 B - FUll working day ------- ")
+empDailyHrsArray.filter(dailyObj=>dailyObj.dailyHours==FULL_TIME_HOURS)
+.forEach(day=>(day.day).toString());
+
+//UC 11 c Part Time woring days
+console.log("UC 11 C Part Time woring day : ");
+let partTiemDays=empDailyHrsArray.filter(EmplyeeObj=>EmplyeeObj.dailyHours==PART_TIME_HOURS)
+.map(EmplyeeObj=>(EmplyeeObj.day).toString());
+console.log(partTiemDays);
+
+
+// UC 11 d. No working days only using Map function
+console.log("UC 11 D No Working day : ");
+let nonWorkingDays=empDailyHrsArray.filter(EmplyeeObj=>EmplyeeObj.dailyHours==0)
+.map(EmplyeeObj=>(EmplyeeObj.day).toString());
+console.log(nonWorkingDays);
