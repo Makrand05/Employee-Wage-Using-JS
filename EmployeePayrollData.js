a2aaeb01ea3a719f5b85c5e72d92742ac46e4c42
Makrand <makrandshingare@gmail.com>
2022-01-23 18:21:29 +0530

    Added : Added gender and Start Date

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,12 +1,19 @@
 class EmployeePayrollData{
+    //property 
     id;
-   // name;
+    name;
     salary;
-    constructor(id,name,salary){
+    gender;
+    startDate;
+    //constructor
+    constructor(id,name,salary,gender,startDate){
         this.id=id;
         this.name=name;
         this.salary=salary;
+        this.gender=gender;
+        this.startDate=startDate;
     }
+    //getter setter 
     get name(){
         return this._name;
     }
@@ -17,6 +24,9 @@ class EmployeePayrollData{
 }
 let empData=new EmployeePayrollData(1,"Ram",60000);
 console.log(empData);
-
+empData.name="Jhon";
+console.log(empData);
+let newEmpData=new EmployeePayrollData(2,"Raajuu",30000,"M",new Date(2022, 01, 01))
+console.log(newEmpData);
 
 
